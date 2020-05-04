305556a7bb50f878c4aa23ab7cf51c3f366ed9e6
Ben Feather <43571410+benfeather@users.noreply.github.com>
2020-05-04 19:58:20 +1200

    Remove invalid line break char

--- scripts/db_backup.sh
+++ scripts/db_backup.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-
 docker exec craft_db /usr/bin/mysqldump --user=user --password=password project > ../backups/database.sql
--- scripts/db_restore.sh
+++ scripts/db_restore.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-
 cat ../backups/database.sql | docker exec -i craft_db /usr/bin/mysql --user=user --password=password project
